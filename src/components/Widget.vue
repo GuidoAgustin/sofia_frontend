03f8ebab1fe41181179857a9a4db1dd107960113
farena <fedarena@gmail.com>
2022-06-22 20:50:55 -0300

    Widget Component

---
+++ src/components/Widget.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card widget">
+    <div class="card-header">
+      <i class="widget-icon" :class="icon" v-if="icon"></i>
+      <h3><slot name="title">Widget Title</slot></h3>
+    </div>
+    <div class="card-body">
+      <slot>Widget Body</slot>
+    </div>
+    <slot name="footer"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<style></style>

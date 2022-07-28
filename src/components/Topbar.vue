8f64fa755641352d8395c8b65985876198bd0750
farena <fedarena@gmail.com>
2022-07-28 20:12:15 -0300

    topbar buttons w/logout

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="topbar">
     <ul class="topbar-buttons">
-      <li><i class="fa-solid fa-bell"></i></li>
-      <li><i class="fa-solid fa-gear"></i></li>
-      <li><i class="fa-solid fa-power-off"></i></li>
+      <li>
+        <i class="fa-solid fa-power-off" @click="$store.dispatch('logout')"></i>
+      </li>
     </ul>
   </div>
 </template>

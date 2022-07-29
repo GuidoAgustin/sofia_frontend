12d0803898497dc8d2f5481d74818f5ae440425c
farena <fedarena@gmail.com>
2022-07-29 10:43:36 -0300

    dashboard fix

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -1,15 +1,17 @@
 <template>
   <h1>DASHBOARD</h1>
   <div class="row">
-    <div class="card">
-      <div class="card-header">Bienvenido!</div>
-      <div class="card-body">
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
-          assumenda similique quae hic eaque, ex cum quaerat beatae odio autem
-          voluptatibus earum quis omnis magni iste ratione officiis veniam
-          doloremque!
-        </p>
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">Bienvenido!</div>
+        <div class="card-body">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit
+            assumenda similique quae hic eaque, ex cum quaerat beatae odio autem
+            voluptatibus earum quis omnis magni iste ratione officiis veniam
+            doloremque!
+          </p>
+        </div>
       </div>
     </div>
   </div>

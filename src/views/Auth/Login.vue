dd782688330c71d7fff8f14d3a5b8c4814c06705
Guido Bianchi <guidobianchi321@gmail.com>
2025-01-09 13:17:38 -0300

    personalizamos el inicio

--- src/views/Auth/Login.vue
+++ src/views/Auth/Login.vue
@@ -2,25 +2,53 @@
   <div class="d-flex align-items-center justify-content-center h-100">
     <div class="card mx-4">
       <div class="card-header text-center">
-        <img src="/img/logo.svg" alt="Logo" class="brand-logo my-3" />
-        <h3 class="m-0">QUARTZ</h3>
-        <small>Content Management System</small>
+        <img src="/img/logosofia.png" alt="Logo Despensa Sofia" class="brand-logo my-3" />
+        <small class="logo-text">Sistema de Gestión</small>
       </div>
       <div class="card-body px-4 pb-4">
-        <FormText label="Email" icon="fa-solid fa-envelope" v-model="form.email" />
-        <FormText
-          label="Password"
-          password
-          icon="fa-solid fa-fingerprint"
-          v-model="form.password"
-          @keyup.enter="signIn"
-        />
+        <div class="input-group" style="position: relative; text-align: right">
+          <div
+            class="input-group-prepend"
+            style="position: absolute; top: 50%; right: 1px; transform: translateY(-50%)"
+          >
+            <span class="input-group-text">
+              <i class="fa fa-envelope" aria-hidden="true"></i>
+            </span>
+          </div>
+          <input
+            type="email"
+            v-model="form.email"
+            class="form-control"
+            placeholder="Ingrese su email "
+          />
+        </div>
+        <div class="input-group">
+  <div class="input-group-prepend" style="position: absolute; top: 56%; right: 25px; transform: translateY(-50%);">
+    <span class="input-group-text">
+      <i class="fa fa-lock" aria-hidden="true"></i>
+    </span>
+  </div>
+  <input
+    :type="mostrarContraseña ? 'text' : 'password'"
+    v-model="form.password"
+    @keyup.enter="signIn"
+    class="form-control"
+    placeholder="Ingrese su contraseña"
+  />
+</div>
+
+        <button type="button" @click="toggleMostrarContraseña" class="btn btn-primary btn-block">
+          {{ mostrarContraseñaTexto }}
+        </button>
         <FormSwitch label="Recordarme" v-model="form.remember" small />
 
-        <button class="btn btn-primary btn-block" @click="signIn">Log In</button>
+        <button class="btn btn-primary btn-block" @click="signIn">Ingresar</button>
+        <button class="btn btn-primary btn-block" @click="$router.push('/registro')">
+          Registrarme
+        </button>
 
         <div class="text-center mt-3">
-          <small> by <a href="http://farenasoft.com" target="_blank"> Farenasoft </a> </small>
+          <small> by GuidoB. </small>
         </div>
       </div>
     </div>
@@ -28,12 +56,10 @@
 </template>
 
 <script>
-import FormText from '@/components/Form/FormText.vue'
 import FormSwitch from '@/components/Form/FormSwitch.vue'
 
 export default {
   components: {
-    FormText,
     FormSwitch
   },
   data: () => ({
@@ -41,7 +67,8 @@ export default {
       email: null,
       password: null,
       remember: true
-    }
+    },
+    mostrarContraseña: false // Estado para mostrar/ocultar la contraseña
   }),
   beforeMount() {
     this.$store.dispatch('setCredentials')
@@ -56,6 +83,11 @@ export default {
     if (default_email) this.form.email = default_email
     if (default_pw) this.form.password = default_pw
   },
+  computed: {
+    mostrarContraseñaTexto() {
+      return this.mostrarContraseña ? 'Ocultar contraseña' : 'Mostrar contraseña'
+    }
+  },
   methods: {
     signIn() {
       this.$store
@@ -64,6 +96,9 @@ export default {
           this.$router.push('/dashboard')
         })
         .catch((err) => this.$toast.error(err))
+    },
+    toggleMostrarContraseña() {
+      this.mostrarContraseña = !this.mostrarContraseña
     }
   }
 }

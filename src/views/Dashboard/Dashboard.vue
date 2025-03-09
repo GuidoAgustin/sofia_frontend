31eb8488dbee987f0ced216142c3d15ddb6976f7
Guido Bianchi <guidobianchi321@gmail.com>
2025-03-09 19:50:35 -0300

    se mejoro la responsividad de los graficos

--- src/views/Dashboard/Dashboard.vue
+++ src/views/Dashboard/Dashboard.vue
@@ -18,100 +18,99 @@
 
     <!-- Botones de navegación -->
     <div class="row mt-2">
-      <div class="col-12 d-flex justify-content-between">
-        <button class="btn btn-primary" @click="$router.push('/ventas')">
+      <div class="col-12 d-flex flex-wrap justify-content-between">
+        <button class="btn btn-primary mb-2" @click="$router.push('/ventas')">
           Ir a la Ventas <i class="fa fa-shopping-basket"></i>
         </button>
-        <button class="btn btn-primary" @click="$router.push('/calculadora')">
+        <button class="btn btn-primary mb-2" @click="$router.push('/calculadora')">
           Ir a la calculadora <i class="fa-solid fa-calculator"></i>
         </button>
-        <button class="btn btn-primary" @click="$router.push('/products')">
+        <button class="btn btn-primary mb-2" @click="$router.push('/products')">
           Ir a la lista de Productos <i class="fa-solid fa-table"></i>
         </button>
-        <button class="btn btn-primary" @click="$router.push('/addProducts')">
+        <button class="btn btn-primary mb-2" @click="$router.push('/addProducts')">
           Agregar Productos <i class="fa-solid fa-store"></i>
         </button>
-        <button class="btn btn-primary" @click="$router.push('/products/scan')">
+        <button class="btn btn-primary mb-2" @click="$router.push('/products/scan')">
           Escanear productos <i class="fa-solid fa-barcode"></i>
         </button>
       </div>
     </div>
 
     <!-- Sección para los gráficos -->
-    <div class="card p-4">
-      <div class="row mt-3">
-        <div class="row">
-          <!-- Donut: Ganancias del día -->
-          <div class="col-md-4 mb-3 d-flex flex-column align-items-center">
-            <apexchart
-              type="donut"
-              :options="donutChartOptions1"
-              :series="donutChartSeries1"
-              width="100%"
-              height="200"
-            ></apexchart>
-            <p class="text-center mt-2">
-              Ganancias del día:
-              <span v-if="totalDia > 0">{{ formatoMoneda(totalDia) }}</span>
-              <span v-else>No se vendió nada</span>
-            </p>
-          </div>
+    <div class="card p-4 mt-3">
+      <div class="row">
+        <!-- Donut: Ganancias del día -->
+        <div class="col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
+          <apexchart
+            type="donut"
+            :options="donutChartOptions1"
+            :series="donutChartSeries1"
+            width="100%"
+            height="200"
+          ></apexchart>
+          <p class="text-center mt-2">
+            Ganancias del día:
+            <span v-if="totalDia > 0">{{ formatoMoneda(totalDia) }}</span>
+            <span v-else>No se vendió nada</span>
+          </p>
+        </div>
 
-          <!-- Donut: Ganancias de la semana -->
-          <div class="col-md-4 mb-3 d-flex flex-column align-items-center">
-            <apexchart
-              type="donut"
-              :options="donutChartOptions2"
-              :series="donutChartSeries2"
-              width="100%"
-              height="200"
-            ></apexchart>
-            <p class="text-center mt-2">
-              Ganancias de la semana:
-              <span v-if="totalSemana > 0">{{ formatoMoneda(totalSemana) }}</span>
-              <span v-else>No se vendió nada</span>
-            </p>
-          </div>
+        <!-- Donut: Ganancias de la semana -->
+        <div class="col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
+          <apexchart
+            type="donut"
+            :options="donutChartOptions2"
+            :series="donutChartSeries2"
+            width="100%"
+            height="200"
+          ></apexchart>
+          <p class="text-center mt-2">
+            Ganancias de la semana:
+            <span v-if="totalSemana > 0">{{ formatoMoneda(totalSemana) }}</span>
+            <span v-else>No se vendió nada</span>
+          </p>
+        </div>
 
-          <!-- Donut: Ganancias del mes -->
-          <div class="col-md-4 mb-3 d-flex flex-column align-items-center">
-            <apexchart
-              type="donut"
-              :options="donutChartOptions3"
-              :series="donutChartSeries3"
-              width="100%"
-              height="200"
-            ></apexchart>
-            <p class="text-center mt-2">
-              Ganancias del mes:
-              <span v-if="totalMes > 0">{{ formatoMoneda(totalMes) }}</span>
-              <span v-else>No se vendió nada</span>
-            </p>
-          </div>
+        <!-- Donut: Ganancias del mes -->
+        <div class="col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
+          <apexchart
+            type="donut"
+            :options="donutChartOptions3"
+            :series="donutChartSeries3"
+            width="100%"
+            height="200"
+          ></apexchart>
+          <p class="text-center mt-2">
+            Ganancias del mes:
+            <span v-if="totalMes > 0">{{ formatoMoneda(totalMes) }}</span>
+            <span v-else>No se vendió nada</span>
+          </p>
+        </div>
 
-          <!-- 1er Gráfico de barras: Productos más vendidos del mes (Top 5) -->
-          <div class="col-md-4 mb-3 d-flex flex-column align-items-center">
-            <apexchart
-              type="bar"
-              :options="barChartOptions1"
-              :series="barChartSeries1"
-              width="100%"
-              height="200"
-            ></apexchart>
-            <p class="text-center mt-2">Productos más vendido del mes</p>
-          </div>
+        <!-- Gráfico de barras: Productos más vendidos del mes (Top 5) - 1/3 del ancho -->
+        <div class="col-12 col-md-5 mb-3 d-flex flex-column align-items-center">
+          <apexchart
+            type="bar"
+            :options="barChartOptions1"
+            :series="barChartSeries1"
+            width="100%"
+            height="200"
+          ></apexchart>
+          <p class="text-center mt-2">Productos más vendidos del mes</p>
+        </div>
 
-          <!-- 2do Gráfico de barras: Cantidad de ventas por mes (año actual) -->
-          <div class="col-md-7 mb-3 d-flex flex-column align-items-center">
-            <apexchart
-              type="bar"
-              :options="barChartOptions2"
-              :series="barChartSeries2"
-              width="250%"
-              height="200"
-            ></apexchart>
-            <p class="text-center mt-2">Ventas por mes</p>
-          </div>
+        <!-- Gráfico de barras: Ventas por mes (año actual) - 2/3 del ancho -->
+        <!-- Se remueve "align-items-center" para permitir que el gráfico se expanda al 100% -->
+        <div class="col-12 col-md-7 mb-3 d-flex flex-column">
+          <apexchart
+            type="bar"
+            :options="barChartOptions2"
+            :series="barChartSeries2"
+            width="100%"
+            height="200"
+          ></apexchart>
+          <p class="text-center mt-2">Ventas por mes</p>
         </div>
       </div>
     </div>
@@ -129,41 +128,50 @@ export default {
   },
   data() {
     return {
-      // Donuts: arrancan en 0
-      donutChartSeries1: [0, 0, 0], // (Tarjeta, Efectivo, Transfer)
+      // Donuts: iniciamos en 0
+      donutChartSeries1: [0, 0, 0],
       donutChartOptions1: {
         chart: { type: 'donut' },
         labels: ['Tarjeta', 'Efectivo', 'Transferencia'],
-        plotOptions: {
-          pie: {
-            donut: { size: '70%' }
+        plotOptions: { pie: { donut: { size: '70%' } } },
+        legend: { position: 'bottom' },
+        responsive: [{
+          breakpoint: 768,
+          options: {
+            chart: { width: '100%' },
+            legend: { position: 'bottom' }
           }
-        },
-        legend: { position: 'bottom' }
+        }]
       },
 
       donutChartSeries2: [0, 0, 0],
       donutChartOptions2: {
         chart: { type: 'donut' },
         labels: ['Tarjeta', 'Efectivo', 'Transferencia'],
-        plotOptions: {
-          pie: {
-            donut: { size: '70%' }
+        plotOptions: { pie: { donut: { size: '70%' } } },
+        legend: { position: 'bottom' },
+        responsive: [{
+          breakpoint: 768,
+          options: {
+            chart: { width: '100%' },
+            legend: { position: 'bottom' }
           }
-        },
-        legend: { position: 'bottom' }
+        }]
       },
 
       donutChartSeries3: [0, 0, 0],
       donutChartOptions3: {
         chart: { type: 'donut' },
         labels: ['Tarjeta', 'Efectivo', 'Transferencia'],
-        plotOptions: {
-          pie: {
-            donut: { size: '70%' }
+        plotOptions: { pie: { donut: { size: '70%' } } },
+        legend: { position: 'bottom' },
+        responsive: [{
+          breakpoint: 768,
+          options: {
+            chart: { width: '100%' },
+            legend: { position: 'bottom' }
           }
-        },
-        legend: { position: 'bottom' }
+        }]
       },
 
       // Totales en pesos (día, semana, mes)
@@ -171,7 +179,7 @@ export default {
       totalSemana: 0,
       totalMes: 0,
 
-      // 1er Gráfico de barras: Productos más vendidos del mes (Top 5)
+      // Gráfico de barras: Productos más vendidos del mes (Top 5)
       barChartSeries1: [
         {
           name: 'Cantidad',
@@ -187,7 +195,6 @@ export default {
         plotOptions: {
           bar: { columnWidth: '90%' }
         },
-        // Habilitamos dataLabels y rotación de etiquetas
         dataLabels: {
           enabled: true,
           offsetY: -20,
@@ -195,10 +202,22 @@ export default {
             fontSize: '12px',
             colors: ['#304758']
           }
-        }
+        },
+        responsive: [{
+          breakpoint: 768,
+          options: {
+            chart: { width: '100%' },
+            xaxis: {
+              labels: {
+                rotate: -45,
+                show: true
+              }
+            }
+          }
+        }]
       },
 
-      // 2do Gráfico de barras: Cantidad de ventas por mes
+      // Gráfico de barras: Ventas por mes (año actual)
       barChartSeries2: [
         {
           name: 'Ventas',
@@ -216,7 +235,14 @@ export default {
         legend: { position: 'bottom' },
         plotOptions: {
           bar: { columnWidth: '90%' }
-        }
+        },
+        responsive: [{
+          breakpoint: 768,
+          options: {
+            chart: { width: '100%' },
+            legend: { position: 'bottom' }
+          }
+        }]
       }
     }
   },
@@ -224,20 +250,18 @@ export default {
     this.cargarVentas()
   },
   methods: {
-    ...mapActions(['getVentas']), // acción en el store que obtiene las ventas
-
+    ...mapActions(['getVentas']),
     formatoMoneda(valor) {
       return new Intl.NumberFormat('es-MX', {
         style: 'currency',
         currency: 'MXN'
       }).format(valor)
     },
-
     async cargarVentas() {
       try {
         const ventas = await this.getVentas()
 
-        // Asegurarnos de que 'venta.productos' sea un array (parsear si es string)
+        // Aseguramos que 'venta.productos' sea un array (parseamos si es string)
         ventas.forEach((venta) => {
           if (typeof venta.productos === 'string') {
             try {
@@ -259,7 +283,7 @@ export default {
         const endOfDay = new Date(startOfDay)
         endOfDay.setHours(23, 59, 59, 999)
 
-        // Rango de la SEMANA (lunes a domingo)
+        // Rango de la SEMANA (de lunes a domingo)
         const startOfWeek = new Date(ahora)
         startOfWeek.setHours(0, 0, 0, 0)
         let day = startOfWeek.getDay()
@@ -269,7 +293,7 @@ export default {
         endOfWeek.setDate(endOfWeek.getDate() + 6)
         endOfWeek.setHours(23, 59, 59, 999)
 
-        // Rango del MES (día 1 al último día)
+        // Rango del MES (del día 1 al último día)
         const startOfMonth = new Date(ahora.getFullYear(), ahora.getMonth(), 1, 0, 0, 0, 0)
         const endOfMonth = new Date(ahora.getFullYear(), ahora.getMonth() + 1, 0, 23, 59, 59, 999)
 
@@ -280,7 +304,7 @@ export default {
         // Productos más vendidos del mes
         const productQuantities = {}
 
-        // Cantidad de ventas por mes (año actual)
+        // Ventas por mes (año actual)
         const monthlySales = new Array(12).fill(0)
         const currentYear = ahora.getFullYear()
 
@@ -312,7 +336,6 @@ export default {
 
             // Acumular productos vendidos este mes
             venta.productos.forEach((prod) => {
-              // Ajusta según tu BD: si usas prod.name_product, prod.name, etc.
               const nombreProd = prod.product || 'Desconocido'
               const cant = Number(prod.cantidad) || 0
               if (!productQuantities[nombreProd]) {
@@ -322,7 +345,7 @@ export default {
             })
           }
 
-          // Cantidad de ventas por mes (año actual)
+          // Ventas por mes (año actual)
           if (fechaVenta.getFullYear() === currentYear) {
             const saleMonth = fechaVenta.getMonth()
             monthlySales[saleMonth] += total
@@ -339,8 +362,7 @@ export default {
         this.totalMes = tarjetaMes + efectivoMes + transMes
 
         // Determinar los 5 productos más vendidos del mes
-        const entries = Object.entries(productQuantities) // [[nombreProd, cantidad], ...]
-        // Ordenar de mayor a menor
+        const entries = Object.entries(productQuantities)
         entries.sort((a, b) => b[1] - a[1])
 
         if (entries.length > 0) {
@@ -348,50 +370,17 @@ export default {
           const top5Names = top5.map(([nombreProd]) => nombreProd)
           const top5Cantidades = top5.map(([, cantidad]) => cantidad)
 
-          // Actualizar primer gráfico de barras con 5 productos
-          this.barChartSeries1 = [
-            {
-              name: 'Cantidad',
-              data: top5Cantidades
-            }
-          ]
-
-          // Para forzar la reactividad y que se re-renderice
+          this.barChartSeries1 = [{ name: 'Cantidad', data: top5Cantidades }]
           this.barChartOptions1 = {
             ...this.barChartOptions1,
             xaxis: {
               ...this.barChartOptions1.xaxis,
               categories: top5Names,
-              labels: {
-                rotate: -45,  // rotar etiquetas 45 grados
-                show: true
-              }
-            },
-            plotOptions: {
-              bar: {
-                columnWidth: '90%',
-                dataLabels: {
-                  position: 'top'
-                }
-              }
-            },
-            dataLabels: {
-              enabled: true,
-              offsetY: -20,
-              style: {
-                fontSize: '12px',
-                colors: ['#304758']
-              }
+              labels: { rotate: -45, show: true }
             }
           }
         } else {
-          // Si no hay productos, poner algo por defecto
-          this.barChartSeries1 = [
-            {
-              name: 'Cantidad',
-              data: [0]
-            }
-          ]
+          this.barChartSeries1 = [{ name: 'Cantidad', data: [0] }]
           this.barChartOptions1 = {
             ...this.barChartOptions1,
             xaxis: {
@@ -401,13 +390,8 @@ export default {
           }
         }
 
-        // Actualizar la serie de 'Ventas por mes' con monthlySales
-        this.barChartSeries2 = [
-          {
-            name: 'Ventas',
-            data: monthlySales
-          }
-        ]
+        // Actualizar la serie de 'Ventas por mes'
+        this.barChartSeries2 = [{ name: 'Ventas', data: monthlySales }]
       } catch (error) {
         console.error('Error cargando ventas:', error)
       }

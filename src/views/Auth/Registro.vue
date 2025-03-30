40a1400df249f14ddb0ddff21214592b4f7774cd
Guido Bianchi <guidobianchi321@gmail.com>
2025-03-30 16:57:16 -0300

    mejorar el formulario de creación de cuenta de administrador con componentes reutilizables

--- src/views/Auth/Registro.vue
+++ src/views/Auth/Registro.vue
@@ -2,56 +2,48 @@
   <div class="d-flex align-items-center justify-content-center h-100">
     <div class="card mx-4">
       <div class="card-body px-4 pb-4">
-        <h1>Crear cuenta de administrador</h1>
-        <form @submit.prevent="crearCuenta">
-          <!-- Campo de Usuario -->
-          <div class="form-group">
-            <label class="form-label">Crear usuario:</label>
-            <input
-              type="text"
-              v-model="formulario.name"
-              placeholder="Ingrese su nombre de usuario"
-              class="form-control"
-            />
-          </div>
-
-          <!-- Campo de Gmail -->
-          <div class="form-group">
-            <label class="form-label">Ingrese su Gmail:</label>
-            <input
-              type="email"
-              v-model="formulario.email"
-              placeholder="Ingrese su Gmail"
-              class="form-control"
-            />
-          </div>
-
-          <!-- Campo de Contraseña -->
-          <div class="form-group">
-            <label class="form-label">Cree una Contraseña:</label>
-            <div class="password-container">
-              <input
-                :type="mostrarContraseña ? 'text' : 'password'"
-                v-model="formulario.password"
-                placeholder="Ingrese su contraseña"
-                class="form-control"
-              />
-              <button
-                type="button"
-                @click="toggleMostrarContraseña"
-                class="btn btn-primary btn-block"
-              >
-                {{ mostrarContraseña ? 'Ocultar contraseña' : 'Mostrar contraseña' }}
-              </button>
-            </div>
-          </div>
-
-          <!-- Botón de Enviar -->
-          <button type="submit" class="btn btn-primary btn-block">Crear cuenta</button>
-        </form>
-        <!-- Botón para Ir Atrás -->
-        <button @click="irAtras" class="btn btn-primary btn-block">Ir atrás</button>
-
+        <h1 class="text-center mb-4">Crear cuenta de administrador</h1>
+        <FormText
+          label="Crear nombre de usuario"
+          v-model="formulario.name"
+          placeholder="Ingrese su nombre de usuario"
+          icon="fa fa-user"
+        />
+        <FormText
+          label="Ingrese su Gmail"
+          v-model="formulario.email"
+          placeholder="Ingrese su Gmail"
+          icon="fa fa-envelope"
+        />
+        <FormText
+          label="Cree una Contraseña"
+          v-model="formulario.password"
+          :password="!mostrarContraseña"
+          placeholder="Ingrese su contraseña"
+          icon="fa fa-lock"
+        >
+          <FormButton
+            :icon="mostrarContraseña ? 'fa fa-eye' : 'fa fa-eye-slash'"
+            @click="toggleMostrarContraseña"
+            class="p-0 m-0"
+          />
+        </FormText>
+        <FormButton
+          class="btn btn-primary btn-block"
+          @click="crearCuenta"
+          theme="primary"
+          block
+        >
+          Crear cuenta
+        </FormButton>
+        <FormButton
+          class="btn btn-secondary btn-block"
+          @click="irAtras"
+          theme="secondary"
+          block
+        >
+          Ir atrás
+        </FormButton>
         <div class="text-center mt-3">
           <small> by GuidoB. </small>
         </div>
@@ -61,7 +53,14 @@
 </template>
 
 <script>
+import FormText from '../../components/Form/FormText.vue'
+import FormButton from '../../components/Form/FormButton.vue'
+
 export default {
+  components: {
+    FormText,
+    FormButton
+  },
   data() {
     return {
       formulario: {
@@ -85,9 +84,7 @@ export default {
         this.$router.push('/login')
       })
     },
-
     toggleMostrarContraseña() {
-      // Cambia el estado de mostrar/ocultar la contraseña
       this.mostrarContraseña = !this.mostrarContraseña
     },
     irAtras() {
